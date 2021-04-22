7636addd5f3fcd0e946c6af3de81479ed5122c91
Anjali Gopinathan <asgopina@usc.edu>
2021-04-21 19:04:29 -0700

    insert

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const people = [];      //empty array of people. says const but stil we can modify it
+
 //Get and post routes
 app.get("/", (req, res) => {
     console.log("In / route!");
-    res.send("Hello world!");
+    // res.send("Hello world!");
+    res.json(people);
+});
+
+//insert endpoint
+app.post("/insert", (req, res) => {
+    const person_name = req.body.person_name;
+    people.push(person_name);
+    res.sendStatus(200);
 });
 
 // anonymous function
