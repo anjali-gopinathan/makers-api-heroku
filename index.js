f01ff470304f1fe9bc177613dd95f4a6a3bb770b
Anjali Gopinathan <asgopina@usc.edu>
2021-04-21 18:41:31 -0700

    minor port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // const constant_var = 10;
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Get and post routes
 app.get("/", (req, res) => {
